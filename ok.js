a7db383492a2f04ea2d7a9aa33d4bab2c81f1746
kadirsalvarci <kadir.salvarci@gmail.com>
2022-05-26 12:59:18 +0200

    new2

--- ok.js
+++ ok.js
@@ -12,12 +12,12 @@ addEventListener() {
   if (styleColor === 'black') {
     styleColor = 'red';
 
-    // 👇️ optionally change tedocument.getElementById('id1').style.color t color
+    //  optionally change tedocument.getElementById('id1').style.color t color
     // btn.style.color = 'white';
   } else {
     styleColor = 'black';
 
-    // 👇️ optionally change tedocument.getElementById('id1').style.color t color
+    // optionally change tedocument.getElementById('id1').style.color t color
     // btn.style.color = 'blue';
   }
 };
@@ -96,11 +96,163 @@ var surname = "salvarci" ;
 document.getElementById("id2").innerHTML= firstname + " " + surname;
 document.getElementById("id2").innerHTML= "degistimi";
 */
-
+/*
 function deneme2(){
   var firstname = "kadir" ;
   var surname = "salvarci" ;
-  son = document.getElementById("id2").innerHTML= firstname + " " + surname;
+  son = document.getElementById("id2").innerHTML= firstname + `ola "tabiki "  bilir `;
   return son;
 }
+
+*/
+/*
+var x,y,z, sum, sub, multi,Div,mod;
+
+  x=70;
+  y=4;
+  sum= x+y ;
+  sub = x-y;
+  Div = x/y ;
+  mod = x%y;
+  //x+=y; // bu rada x y kadar 
+  x-=y;
+  txt1 = "ilk test ";
+  txt1+= "son olan bu" ;
+  //txtrslt = txt1;
+   if (x<y && x
+
+  document.getElementById("id1").innerHTML = txtrslt;
+
+  */
+
+/*
+function mouseMove (e){
+
+  var x = e.clientX;
+  var y = e.clientY;
+  document.getElementById("hesap").innerHTML= "x kordinati :"+ x + " y kordinati :"+ y ;
+}
+
+
+*/
+/*
+work2();
+function work2(){
+
+  var text ="ali,mehmet, kadir, burak , beyza";
+  f = document.getElementById("hesap").innerHTML=text.slice(10,21);
+  document.write.f;
+}
+*/
+
+//work2();
+function lowcs(){
+
+  var text ="ali,mehmet, Kadir, burak , beyza";
+  document.getElementById("hesap").innerHTML=text.toLowerCase();
+ 
+}
+//lowcs();
+//upcs();
+/*
+
+function upcs(){
+
+  var text ="ali,mehmet, Kadir, burak , beyza";
+  document.getElementById("hesap").innerHTML=text.toUpperCase();
+ 
+}
+
+function upcs(){
+
+  var text ="ali,mehmet, Kadir, burak , beyza";
+  document.getElementById("hesap").innerHTML=text.toUpperCase();
+ 
+}
+*/
+
+/*
+
+
+//nmbr();
+function nmbr(){
+
+  var a = 12e4;
+  var b = 12e-4
+  document.getElementById("hesap").innerHTML=a,b;
+}
+*/
+//nmbr();
+function nmbr(){
+
+  var a = 70 / 0;
+  var b = -70/0;
+  document.getElementById("hesap").innerHTML=typeof a;
+}
+/*
+//show();
+function show(){
+  var d = new Date;
+  document.getElementById("hesap").innerHTML= parseInt("3,60");
+  document.getElementById("hesap").innerHTML= d;
+
+
+
+}
+
+*/
+/*
+var d = new Date;
+var tdays =["pazar", "pazartesi", "sali", "carsamba", "persembe", "cuma", "cumartesi"] // array / diziler
+var pc={ok:"kitap", ok2:"kitap2", ok3:"kitap3", ok4:"kitap4"} // object -- nesne 
+document.getElementById("hesap").innerHTML=tdays[d.getDay()];                 //tdays[6];
+document.getElementById("hesap").innerHTML=tdays[3];  
+document.getElementById("hesap").innerHTML=pc.ok4;// pc["ok3"];
+*/
+/*
+function verify(){
+  var v = 7;
+  var c = 9;
+  document.getElementById("result").style.color="green";
+  document.getElementById("result").innerHTML=(v==10);
+  document.getElementById("result").innerHTML=(c==9);
+  
+
+
+}
+*/
+/*
+var notes= 67, message = "";
+
+function bilgi(){
+
+  if (notes >= 60) {
+    message = "gectin"
+  }else {
+    message= "kaldin"
+  }
+  alert(message);
+
+}
+
+*/
+/*
+function sSayi(){
+
+  var i,text="";
+  
+  for(i=0;i<10; i){
+    
+    text += i;
+  } 
+  console.log.text;
+  document.getElementById("result").innerHTML=text;
+}
+
+*/
+
+//var arrayss=[1,2,3,4,5,6];
+
+
+
 
